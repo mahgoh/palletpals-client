4f0f95bcaa6b82ef99e1a6500a2e68af25bc100f
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-04-13 21:45:10 +0200

    Change gray to zinc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
       sans: ['Inter var', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.zinc,
+      },
+    },
   },
   plugins: [],
 }
