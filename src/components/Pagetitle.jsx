051fa92b1fcc09610b249392433fb3f3b0acc017
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-04-15 19:55:50 +0200

    Remove custom font

--- src/components/Pagetitle.jsx
+++ src/components/Pagetitle.jsx
@@ -1,7 +1,9 @@
 export default function Pagetitle({ children }) {
   return (
     <div>
-      <h1 className="text-4xl font-extrabold">{children}</h1>
+      <h1 className="text-4xl font-extrabold leading-10 tracking-tight sm:text-6xl sm:leading-none lg:text-5xl">
+        {children}
+      </h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,6 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    fontFamily: {
-      sans: ['Inter var', 'sans-serif'],
-    },
     extend: {
       colors: {
         gray: colors.zinc,
