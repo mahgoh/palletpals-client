9924e0db4040e811df8515a7f01b03e68f36487c
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-04-18 21:41:06 +0200

    Allow to close debug

--- src/components/Debug/index.jsx
+++ src/components/Debug/index.jsx
@@ -1,22 +1,36 @@
+import { useState } from 'react'
+import { ClipboardCopyIcon } from '@heroicons/react/outline'
 import Button from '@/components/Button'
-import { useTranslation } from 'react-i18next'
 
 export default function Debug({ data }) {
-  const { t } = useTranslation()
+  const [show, setShow] = useState(true)
 
   function copy() {
     navigator.clipboard.writeText(JSON.stringify(data))
   }
 
+  function close() {
+    setShow(false)
+  }
+
   if (GLOBAL.DEBUG) {
     return (
-      <div className="relative my-10 bg-rose-50 p-6 pt-12 text-rose-500">
-        <div className="absolute top-4 left-4 text-sm uppercase">Debug</div>
-        <Button color="red" className="absolute top-4 right-4" onClick={copy}>
-          {t('common.copy')}
-        </Button>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
+      show && (
+        <div className="relative my-10 bg-rose-50 p-6 pt-12 text-rose-500">
+          <div className="absolute top-4 left-4 text-sm uppercase">Debug</div>
+          <div className="absolute top-4 right-4 flex space-x-2">
+            <Button color="red" onClick={copy}>
+              <ClipboardCopyIcon className="h-5 w-5 " aria-hidden="true" />
+            </Button>
+            <Button color="red" onClick={close}>
+              Close
+            </Button>
+          </div>
+          <pre className="text-sm leading-tight">
+            {JSON.stringify(data, null, 2)}
+          </pre>
+        </div>
+      )
     )
   } else {
     return null
