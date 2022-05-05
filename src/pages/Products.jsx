7ba14a7c499181258cdc93fcc514f60f0bb149fd
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-05-05 13:44:17 +0200

    Add dynamic products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,51 +1,22 @@
 import { useTranslation } from 'react-i18next'
+import { Product } from '@/services/api'
 import Debug from '@/components/Debug'
 import Main from '@/components/Main'
 import Pagetitle from '@/components/Pagetitle'
 import ProductList from '@/components/ProductList'
 import Spacer from '@/components/Spacer'
 
-const products = [
-  {
-    id: 1,
-    name: 'DeWalt Saw Machine',
-    price: 249,
-    image: '/images/dewalt.jpg',
-  },
-  {
-    id: 2,
-    name: 'Cover Suspension System',
-    price: 89,
-    image: '/images/suspension.jpg',
-  },
-  {
-    id: 3,
-    name: 'Focus Paper Refill',
-    price: 56.9,
-    image: '/images/paper.jpg',
-  },
-  {
-    id: 4,
-    name: 'Machined Mechanical Steel',
-    price: 194,
-    image: '/images/steel.jpg',
-  },
-  {
-    id: 5,
-    name: 'CNC Mill',
-    price: 99,
-    image: '/images/cnc.jpg',
-  },
-]
-
 export default function Products() {
   const { t } = useTranslation()
+  const { products, error, loading } = Product.all()
+
   return (
     <Main>
       <Pagetitle title={t('common.products')} />
-      <Debug data={products} />
+      <Debug data={{ products, error }} />
       <Spacer size="lg" />
-      <ProductList products={products} />
+      {loading && <div>Loading...</div>}
+      {!loading && <ProductList products={products} />}
     </Main>
   )
 }
