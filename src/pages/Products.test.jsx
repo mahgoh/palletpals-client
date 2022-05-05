757b30f1d663c84fbbc19a03878ccdc93bd7b169
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-05-05 16:07:40 +0200

    Add products test

---
+++ src/pages/Products.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@/utils/test-utils'
+import Products from './Products'
+import { products } from '@/mocks/fixtures/products'
+
+describe('page: Products', () => {
+  it('should render', () => {
+    render(<Products />, { renderer: 'complete' })
+    setTimeout(() => {
+      products.forEach((product) => {
+        expect(screen.getByText(product.name)).not.toBeFalsy()
+        expect(screen.getByText(product.description)).not.toBeFalsy()
+        expect(screen.getByText(product.price)).not.toBeFalsy()
+      })
+    }, 200)
+  })
+})
