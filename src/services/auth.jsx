1e1a3332dfdafc9efee35c132d982f6dd7f0042e
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-05-24 13:49:33 +0200

    Fix redirect on page reload on protected routes

--- src/services/auth.jsx
+++ src/services/auth.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react'
-import { useLocation, Navigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { User } from '@/services/api'
 
 let AuthContext = createContext()
@@ -48,18 +48,21 @@ export function AuthProvider({ children }) {
 export function RequireAuth({ children }) {
   let auth = useAuth()
   let location = useLocation()
+  let navigate = useNavigate()
 
   useEffect(() => {
-    auth.validate()
+    async function validate() {
+      await auth.validate()
+      if (!auth.authenticated) {
+        // Redirect them to the /login page, but save the current location they were
+        // trying to go to when they were redirected. This allows us to send them
+        // along to that page after they login, which is a nicer user experience
+        // than dropping them off on the home page.
+        navigate('/login', { replace: true, state: { from: location } })
+      }
+    }
+    validate()
   }, [])
-
-  if (!auth.authenticated) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
-    return <Navigate to="/login" state={{ from: location }} replace />
-  }
 
   return children
 }
