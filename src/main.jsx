3e9bcf490a0b89c1d194cc275537b933f825e950
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-05-24 17:17:16 +0200

    Use Loader

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from '@/services/auth'
+import Loader from '@/components/Loader'
 import Router from '@/Router'
 import '@/i18n'
 import '@/index.css'
@@ -9,7 +10,7 @@ const container = document.getElementById('root')
 const root = createRoot(container)
 
 root.render(
-  <Suspense fallback="loading">
+  <Suspense fallback={<Loader />}>
     <AuthProvider>
       <Router />
     </AuthProvider>
