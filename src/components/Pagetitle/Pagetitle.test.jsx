11e79119b844c5ccecabf736b650ffeac02031cb
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-05-28 15:22:27 +0200

    Allow children in pagetitle

--- src/components/Pagetitle/Pagetitle.test.jsx
+++ src/components/Pagetitle/Pagetitle.test.jsx
@@ -8,13 +8,13 @@ describe('pagetitle', () => {
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Hello')
   })
 
-  it('should not render children', () => {
-    render(
+  it('should render children', () => {
+    const { container } = render(
       <Pagetitle title="Hello">
-        <div>should not be rendered</div>
+        <div>should be rendered</div>
       </Pagetitle>
     )
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Hello')
-    expect(screen.getByRole('heading', { level: 1 })).toMatchSnapshot()
+    expect(container).toMatchSnapshot()
   })
 })
--- src/components/Pagetitle/index.jsx
+++ src/components/Pagetitle/index.jsx
@@ -1,13 +1,28 @@
 import PropTypes from 'prop-types'
 
-export default function Pagetitle({ title }) {
-  return (
-    <div>
-      <h1 className="text-4xl font-extrabold leading-10 tracking-tight sm:text-6xl sm:leading-none lg:text-5xl">
-        {title}
-      </h1>
-    </div>
-  )
+export default function Pagetitle({ title, children }) {
+  function render() {
+    return (
+      <div>
+        <h1 className="text-4xl font-extrabold leading-10 tracking-tight sm:text-6xl sm:leading-none lg:text-5xl">
+          {title}
+        </h1>
+      </div>
+    )
+  }
+
+  function renderWithChildren() {
+    return (
+      <div className="flex items-center justify-between">
+        <h1 className="text-4xl font-extrabold leading-10 tracking-tight sm:text-6xl sm:leading-none lg:text-5xl">
+          {title}
+        </h1>
+        {children}
+      </div>
+    )
+  }
+
+  return children ? renderWithChildren() : render()
 }
 
 Pagetitle.propTypes = {
