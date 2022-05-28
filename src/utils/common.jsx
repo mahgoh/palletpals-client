7ad2b99a82e7eef18cedb8cbd3d8a0f3f43aadf6
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-05-28 15:04:36 +0200

    Add parseDateTime

--- src/utils/common.jsx
+++ src/utils/common.jsx
@@ -5,3 +5,16 @@ export function classNames(...classes) {
 export function productImageURL(imageId) {
   return `${GLOBAL.API_URL}/product-images/${imageId}`
 }
+
+export function parseDateTime(dateTimeString, locale = 'en') {
+  const datetime = new Date(dateTimeString)
+  const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: '2-digit',
+  }
+
+  return datetime.toLocaleString(locale, options)
+}
