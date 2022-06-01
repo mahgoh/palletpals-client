fa431dad798167f05d5f7b642f5cb4e14eeb34cc
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-06-01 22:38:01 +0200

    Make loader an asset instead of public

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,7 +1,9 @@
+import loader from '@/assets/loader.svg'
+
 export default function Loader() {
   return (
     <div className="flex grow items-center justify-center py-4">
-      <img className="h-16 w-16" src="/loader.svg" />
+      <img className="h-16 w-16" src={loader} />
     </div>
   )
 }
